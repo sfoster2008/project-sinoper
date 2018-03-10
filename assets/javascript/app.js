edf9eda93a64ed0e69348808807ff4ce36516df0
hurricaneronron <vprzybyl@gmail.com>
2018-03-10 08:24:01 -0800

    initial javascript pseudo

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+// get user input (city name)
+// on click "search"
+
+// get user input (date)
+// datepicker or simple user entry
+
+// MAP
+
+// get user input (lodging name/address)
+// on click map info window
+  // three options: add to lodging / add to activities / add to meals
+
+// display user inputs
+// three divs: info / activities / meals - added to html according to user differentiation
+  // info div:
+      // lodging name & address & $ / date
+      // cost: average of $ ratings from Yelp that updates as items are added (assign values 1-4 to the dollar signs, get average)
+// maintain this info so user can then pick a new day to plan
+  // display new user inputs, same as above
+  // auto fill hotel info
+
+// ability to remove individual items
+  // button [X] - on click, remove div
+  // update avg $
+
+// totals div
+  // avg $ for lodging / meals / activities & total avg
+
+// text to accompany cost averages (just for fun)
